aa90b397ac824ab75ea6272c5c49ab2f8f0cbe32
HanSolo71 <HanSolo552@gmail.com>
2018-10-29 13:25:03 -0400

    Add files via upload

---
+++ Veeam Job Dump.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module VMware.VimAutomation.Cis.Core
+Import-Module VMware.VimAutomation.Common
+Import-Module VMware.VimAutomation.Core
+
+
+Connect-VIServer vcenter.corp.com
+$DailyVeeamJob = Get-VM | Get-Annotation -Name Veeam | Sort-Object
+$ArchiveVeeamJob = Get-VM | Get-Annotation -Name "Veeam Archive" | Sort-Object
+
+$DailyVeeamJob | Out-GridView
+$ArchiveVeeamJob | Out-GridView
